5e43d091a943fb8949830af1261ddc0664dc4f00
Acechengui <623169670@qq.com>
2023-03-21 08:52:25 +0800

    优化弹窗后导航栏偏移的问题

--- jovefast-UI/src/layout/components/AppMain.vue
+++ jovefast-UI/src/layout/components/AppMain.vue
@@ -49,11 +49,3 @@ export default {
 }
 </style>
 
-<style lang="scss">
-// fix css style bug in open el-dialog
-.el-popup-parent--hidden {
-  .fixed-header {
-    padding-right: 17px;
-  }
-}
-</style>
