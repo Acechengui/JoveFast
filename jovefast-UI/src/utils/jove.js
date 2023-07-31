c09cf4f85640cf5755130bad89a510de3a05fda6
Acechengui <623169670@qq.com>
2023-07-31 15:49:46 +0800

    修订

--- jovefast-UI/src/utils/jove.js
+++ jovefast-UI/src/utils/jove.js
@@ -45,6 +45,44 @@ export function parseTime(time, pattern) {
   return time_str
 }
 
+
+//时间格式化:年-月-日
+export function formatDateC(param) {
+  let dtc = new Date(param)
+  //获取月,默认月份从0开始
+  let dtuMonth = dtc.getMonth() + 1
+  //获取日
+  let dtuDay = dtc.getDate()
+  //处理1-9月前面加0
+  if (dtuMonth < 10) {
+    dtuMonth = "0" + (dtc.getMonth() + 1)
+  }
+  //处理1-9天前面加0
+  if (dtuDay < 10) {
+    dtuDay = "0" + dtc.getDate()
+  }
+  //获取小时
+  let dtuHours = dtc.getHours()
+  //处理1-9时前面加0
+  if (dtuHours < 10) {
+    dtuHours = "0" + dtc.getHours()
+  }
+  //获取分钟
+  let dtuMinutes = dtc.getMinutes()
+  //处理1-9分前面加0
+  if (dtuMinutes < 10) {
+    dtuMinutes = "0" + dtc.getMinutes()
+  }
+  //获取秒
+  let dtuSeconds = dtc.getSeconds()
+  //处理1-9秒前面加0
+  if (dtuSeconds < 10) {
+    dtuSeconds = "0" + dtc.getSeconds()
+  }
+  //组装年月日,按自己的要求来
+  return dtc.getFullYear() + "-" + dtuMonth + "-" + dtuDay
+}
+
 // 表单重置
 export function resetForm(refName) {
   if (this.$refs[refName]) {
