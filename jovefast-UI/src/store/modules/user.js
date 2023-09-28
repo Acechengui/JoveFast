78315208f8c84dd71a991691dcc3467ce3079a25
Acechengui <623169670@qq.com>
2023-09-28 11:37:51 +0800

    全局数据存储用户编号

--- jovefast-UI/src/store/modules/user.js
+++ jovefast-UI/src/store/modules/user.js
@@ -4,6 +4,7 @@ import { getToken, setToken, setExpiresIn, removeToken } from '@/utils/auth'
 const user = {
   state: {
     token: getToken(),
+    id: '',
     name: '',
     nickname:'',
     avatar: '',
@@ -17,6 +18,9 @@ const user = {
     },
     SET_EXPIRES_IN: (state, time) => {
       state.expires_in = time
+    },
+    SET_ID: (state, id) => {
+      state.id = id
     },
     SET_NAME: (state, name) => {
       state.name = name
@@ -68,6 +72,7 @@ const user = {
           } else {
             commit('SET_ROLES', ['ROLE_DEFAULT'])
           }
+          commit('SET_ID', user.userId)
           commit('SET_NAME', user.userName)
           commit('SET_NICK_NAME', user.nickName)
           commit('SET_AVATAR', avatar)
@@ -90,7 +95,7 @@ const user = {
         })
       })
     },
-    
+
     // 退出系统
     LogOut({ commit, state }) {
       return new Promise((resolve, reject) => {
