296a52fe31093b356fb45d8d9a2fbeab5d9c3a1f
acechengui <623169670@qq.com>
2023-11-03 01:52:30 +0800

    feat:补充信息

---
+++ jovefast-UI/src/views/flowable/task/process/send/flow.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <flow-view :flowData="flowData"/>
+    </div>
+  </template>
+  <script>
+  import FlowView from './flowview'
+  
+  export default {
+    name: "Flow",
+    components: {
+      FlowView
+    },
+    props: {
+      flowData: {
+        type: Object,
+        default: () => {}
+      },
+    },
+    data() {
+      return {};
+    }
+  };
+  </script>
+  
