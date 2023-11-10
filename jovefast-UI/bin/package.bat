a1e0c373cf38ae98dee2d2cf0ac94311ce0000f3
acechengui <623169670@qq.com>
2023-11-10 22:53:59 +0800

    chore: 构建脚本调整

--- jovefast-UI/bin/package.bat
+++ jovefast-UI/bin/package.bat
@@ -1,12 +1,12 @@
 @echo off
 echo.
-echo [信息] 安装Web工程，生成node_modules文件。
+echo [信息] 打包Web工程，生成dist文件。
 echo.
 
 %~d0
 cd %~dp0
 
 cd ..
-npm install --registry=https://registry.npmmirror.com
+npm run build:prod
 
 pause
